a48963815ba5c2fe0f8bf20b079950f96b485521
Wesley <sunbeyond97@163.com>
2026-03-29 16:35:14 +0800

    feat: set default and minimum window size for JVM

--- composeApp/src/jvmMain/kotlin/top/iwesley/lyn/music/main.kt
+++ composeApp/src/jvmMain/kotlin/top/iwesley/lyn/music/main.kt
@@ -1,16 +1,28 @@
 package top.iwesley.lyn.music
 
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import java.awt.Dimension
 import top.iwesley.lyn.music.platform.createJvmAppComponent
 
 fun main() = application {
     val appComponent = remember { createJvmAppComponent() }
+    val windowState = rememberWindowState(
+        size = DpSize(1440.dp, 900.dp),
+    )
     Window(
         onCloseRequest = ::exitApplication,
         title = "LynMusic",
+        state = windowState,
     ) {
+        SideEffect {
+            window.minimumSize = Dimension(1100, 720)
+        }
         App(appComponent)
     }
 }
